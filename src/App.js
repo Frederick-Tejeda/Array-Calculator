ddef7023909c2fe49c43dcf0d42347d4b5494a84
Frederick-Tejeda <ffrederf3@gmail.com>
2023-12-08 15:23:34 -0400

    Project finished!

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import * as math from 'mathjs'
 
-	var arrayA = [], arrayB = [];
-	var lengthArrayA = [undefined, undefined], lengthArrayB = [undefined, undefined];
-	var operation = '+';
+var arrayA = [], arrayB = [];
+var lengthArrayA = [undefined, undefined], lengthArrayB = [undefined, undefined];
+var operation = '+';
 
 function App() {
 	
 	const [newArrayA, setNewArrayA] = useState([])
+	const [isArrayACreated, setIsArrayACreated] = useState(false);
 	const [newArrayB, setNewArrayB] = useState([])
+	const [isArrayBCreated, setIsArrayBCreated] = useState(false);
 	const [newArrayC, setNewArrayC] = useState([])
 	const [operationString, setOperationString] = useState('')
 	const [operationResult, setOperationResult] = useState('')
@@ -23,12 +25,17 @@ function App() {
 			for(let y = 0; y <= Number(lengthArrayA[0]) - 1; y++){
 				arrayA[y] = []
 				for(let x = 0; x <= Number(lengthArrayA[1]) - 1; x++){
-					arrayA[y][x] = '0'
+					if(isArrayACreated){
+						arrayA[y][x] = (y >= newArrayA.length || x >= newArrayA[0].length) ? '0' : String(newArrayA[y][x])
+					}
+					else{
+						arrayA[y][x] = '0';
+
+					}
+					setIsArrayACreated(true)
 				}
-				console.log('arrayA', arrayA)
 			}
 			setNewArrayA(arrayA)
-			console.log(`arrayA: ${arrayA} || newArrayA: ${newArrayA}`)
 			CreateArrayC()
 		}catch(err){
 			console.log(err)
@@ -44,12 +51,15 @@ function App() {
 			for(let y = 0; y <= Number(lengthArrayB[0]) - 1; y++){
 				arrayB[y] = []
 				for(let x = 0; x <= Number(lengthArrayB[1]) - 1; x++){
-					arrayB[y][x] = 0
+					if(isArrayBCreated){
+						arrayB[y][x] = (y >= newArrayB.length || x >= newArrayB[0].length) ? '0' : String(newArrayB[y][x])
+					}else{
+						arrayB[y][x] = '0';
+					}
+					setIsArrayBCreated(true)
 				}
-				console.log('arrayB', arrayB)
 			}
 			setNewArrayB(arrayB)
-			console.log(`arrayB: ${arrayB} || newArrayB: ${newArrayB}`)
 			CreateArrayC()
 		}catch(err){
 			console.log(err)
@@ -59,20 +69,15 @@ function App() {
 	const CreateArrayC = () => {
 		
 		try{
-			console.log('inputs', document.getElementsByClassName('input'));
-			
+
 			if(Number(lengthArrayB[0]) && Number(lengthArrayB[1]) && Number(lengthArrayA[0]) && Number(lengthArrayA[1]) && (Number(lengthArrayB[1]) > 0 && Number(lengthArrayB[1]) > 0 && Number(lengthArrayA[0]) > 0 && Number(lengthArrayA[1]) > 0)){ 
 				
-				console.log(`arrayA: ${arrayA} || arrayB: ${arrayB}`)
-				
-				const mA = math.matrix(arrayA);
-				const mB = math.matrix(arrayB);	
+				const [mA, mB] = [math.matrix(arrayA), math.matrix(arrayB)];	
 				
 				if(lengthArrayA[0] === lengthArrayB[0]){
 					if(lengthArrayA[1] === lengthArrayB[1]){
 						try{
-						setNewArrayC((operation === '+') ? math.add(mA, mB) : (operation === '-') ? math.subtract(mA, mB) : (operation === 'x') ? math.multiply(mA, mB) : math.divide(mA, mB)); 
-						console.log((operation === '+') ? math.add(mA, mB) : (operation === '-') ? math.subtract(mA, mB) : (operation === 'x') ? math.multiply(mA, mB) : math.divide(mA, mB))
+						setNewArrayC((operation === '+') ? math.add(mA, mB) : (operation === '-') ? math.subtract(mA, mB) : math.multiply(mA, mB)); 
 						}
 						catch(err){
 							console.log(err)
@@ -86,15 +91,12 @@ function App() {
 				if(lengthArrayA[0] === lengthArrayB[1]){
 					if(lengthArrayA[1] === lengthArrayB[0]){
 						try{
-						setNewArrayC((operation === '+') ? math.add(mA, mB) : (operation === '-') ? math.subtract(mA, mB) : (operation === 'x') ? math.multiply(mA, mB) : math.divide(mA, mB)); 
-						console.log((operation === '+') ? math.add(mA, mB) : (operation === '-') ? math.subtract(mA, mB) : (operation === 'x') ? math.multiply(mA, mB) : math.divide(mA, mB))
+						setNewArrayC((operation === '+') ? math.add(mA, mB) : (operation === '-') ? math.subtract(mA, mB) : math.multiply(mA, mB)); 
 						}
 						catch(err){
 							console.log(err)
-							console.log(`arrayA: ${arrayA} || arrayB: ${arrayB}`)
 							setNewArrayC({"_data": "Invalid operation"})
-						}
-						
+						}	
 					}
 					return
 				}
@@ -108,7 +110,6 @@ function App() {
 		let currentArray = newArrayA
 		currentArray[y][x] = (data === '') ? '0' : data
 		setNewArrayA(currentArray)
-		console.log('currentArray', currentArray)
 		CreateArrayC()
 	}
 	
@@ -116,19 +117,16 @@ function App() {
 		let currentArray = newArrayB
 		currentArray[y][x] = (data === '') ? '0' : data
 		setNewArrayB(currentArray)
-		console.log('currentArray', currentArray)
 		CreateArrayC()
 	}
 	
 	const SetOperation = (e) => {
 		operation = e.target.value; 
 		CreateArrayC(); 
-		console.log(e.target.value)
 	}
 	
 	useEffect(() => {
-		setOperationString((operation === '+') ? 'A+B' : (operation === '-') ? 'A-B' : (operation === 'x') ? 'AB' : 'A/B')
-		console.log("newArrayC['_size[0]']", newArrayC['_size[0]'])
+		setOperationString((operation === '+') ? 'A + B' : (operation === '-') ? 'A - B' : 'AB')
 		setOperationResult((newArrayC['_size'] !== undefined) ? `${JSON.stringify(newArrayC._size[0])} x ${JSON.stringify(newArrayC._size[1])}` : '')
 	}, [operation, newArrayC])
 	
@@ -139,30 +137,28 @@ function App() {
 			<section id="Arrays">
 				<article>
 					<div id="ArrayA" className="ArrayA-B" style={{height: '100%'}}>
-						{arrayA.map((y, keyY) => (<div key={keyY}>{y.map((x, keyX) => (<input key={keyX} className="input" placeholder={x} onChange={(e) => AddToArrayA(e.target.value, keyY, keyX)}/>))}</div>))}
-						{/*JSON.stringify(newArrayA)*/}
+						{arrayA.map((y, keyY) => (<div key={keyY}>{y.map((x, keyX) => (<input key={keyX} className="inputA" placeholder={x} onChange={(e) => AddToArrayA(e.target.value, keyY, keyX)}/>))}</div>))}
 					</div>
 				</article>
 				<article>
 					<div id="ArrayB" className="ArrayA-B">
-						{arrayB.map((y, keyY) => (<div key={keyY}>{y.map((x, keyX) => (<input key={keyX} className="input" placeholder={x} onChange={(e) => AddToArrayB(e.target.value, keyY, keyX)}/>))}</div>))}
-						{/*JSON.stringify(newArrayB)*/}
+						{arrayB.map((y, keyY) => (<div key={keyY}>{y.map((x, keyX) => (<input key={keyX} className="inputB" placeholder={x} onChange={(e) => AddToArrayB(e.target.value, keyY, keyX)}/>))}</div>))}
 					</div>
 				</article>
 			</section>
 			<section id="input-container">
 				<article>
 					<label>{'Matrix A '}
-						<input type="text" placeholder="3" onChange={(e) => {lengthArrayA[0] = e.target.value; console.log('lengthArrayA', lengthArrayA); CreateArrayA()}} />
+						<input type="text" placeholder="3" onChange={(e) => {lengthArrayA[0] = e.target.value; CreateArrayA()}} />
 						{' X '}
-						<input type="text" placeholder="2" onChange={(e) => {lengthArrayA[1] = e.target.value; console.log('lengthArrayA', lengthArrayA); CreateArrayA()}} />
+						<input type="text" placeholder="2" onChange={(e) => {lengthArrayA[1] = e.target.value; CreateArrayA()}} />
 					</label>
 				</article>
 				<article>
 					<label>{'Matrix B '}
-						<input type="text" placeholder="3" onChange={(e) => {lengthArrayB[0] = e.target.value; console.log('lengthArrayB', lengthArrayB); CreateArrayB()}} />
+						<input type="text" placeholder="3" onChange={(e) => {lengthArrayB[0] = e.target.value; CreateArrayB()}} />
 						{' X '}
-						<input type="text" placeholder="2" onChange={(e) => {lengthArrayB[1] = e.target.value; console.log('lengthArrayB', lengthArrayB); CreateArrayB()}} />
+						<input type="text" placeholder="2" onChange={(e) => {lengthArrayB[1] = e.target.value; CreateArrayB()}} />
 					</label>
 				</article>
 			</section>
@@ -171,8 +167,7 @@ function App() {
 					<p>Operation: </p> <select onChange={(e) => SetOperation(e)}>
 						<option value="+">+</option>
 						<option value="-">-</option>
-						<option value="x">x</option>
-						<option value="/">/</option>
+						<option value="x">x</option>	
 					</select>
 				</div>
 			<hr />
